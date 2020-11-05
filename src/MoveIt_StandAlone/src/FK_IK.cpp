bf810b584abd0624d37031600347e831ed442314
jwatson-CO-edu <james.watson-2@colorado.edu>
2020-11-04 21:25:32 -0700

    tomorrow plan

---
+++ src/MoveIt_StandAlone/src/FK_IK.cpp
@@ -0,0 +1,11 @@
+/*
+~~~ DEV PLAN ~~~
+[ ] Config file
+[ ] FK Service
+    [ ] Test
+[ ] IK Service
+    [ ] Test
+[ ] Test: Feed FK to IK
+    [ ] Find out if I had to change the input pose to the robot basis somehow
+[ ] Confer with Davis to test ROS Bridge
+*/
